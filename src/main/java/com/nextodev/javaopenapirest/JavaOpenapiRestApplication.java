42652f3ac8299c0795b09d50700de92f26d3bbd8
filipbosnjak <filipo.bosnjak@gmail.com>
2022-12-23 11:10:15 +0100

    initial

---
+++ src/main/java/com/nextodev/javaopenapirest/JavaOpenapiRestApplication.java
@@ -0,0 +1,13 @@
+package com.nextodev.javaopenapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaOpenapiRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaOpenapiRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nextodev/javaopenapirest/JavaOpenapiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nextodev.javaopenapirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaOpenapiRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
